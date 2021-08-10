095662c56b3cfdb1f2e0bfdfa16c965657994be5
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-08-10 16:34:33 +0530

    initial setup

---
+++ android/app/src/main/kotlin/com/rgtechcafe/custom_painter_roughpad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rgtechcafe.custom_painter_roughpad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:custom_painter_roughpad/Screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rough Pad',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
