842f7eb8cbad98267fb171a93eb395eab0138d48
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-08-10 16:54:26 +0530

    color picker

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,12 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final List<Color> colors = [
+    Colors.red,
+    Colors.yellow,
+    Colors.black,
+    Colors.blue,
+    Colors.purple,
+    Colors.orange,
+    Colors.cyan,
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
+      body: Container(
+        height: mediaQuery.height,
+        width: mediaQuery.width,
+        color: Colors.white,
+        child: const Center(
+          child: Text("Drawing"),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 10,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.pinkAccent.withOpacity(0.3),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(
+              colors.length,
+              (index) => ColorPicker(
+                color: colors[index],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorPicker extends StatelessWidget {
+  final Color color;
+
+  const ColorPicker({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: color,
+      radius: 20,
     );
   }
 }
